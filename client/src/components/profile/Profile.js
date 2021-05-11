2dc7467d3b43f998f421703cb2d665ac4cd772ff
devkcodes <devanshkatheria27@gmail.com>
2021-05-11 08:19:21 +0530

    new commit

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
